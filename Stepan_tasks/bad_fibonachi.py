f16167778a9da8866b75bacd400ba78b2c24c52a
NikitaFedyanin <40120863+NikitaFedyanin@users.noreply.github.com>
2019-08-02 23:27:21 +0400

    новый задачи

---
+++ Stepan_tasks/bad_fibonachi.py
@@ -0,0 +1,21 @@
+"""
+Последовательность f0, f1, f2, … задана рекуррентными соотношениями
+f0 = 0, f1 = 1, f2 = 2 и fk = fk-1 + fk-3.
+
+Напишите программу, которая вычисляет n элементов этой последовательности c номерами k1, k2, …, kn.
+"""
+
+
+def wrong_fibonachi(item):  # так же как фибоначчи, только другой шаг
+    if item == 0 or item == 1 or item == 2:
+        return item
+    else:
+        return wrong_fibonachi(item - 1) + wrong_fibonachi(item - 3)
+
+
+inp = [int(i) for i in input().split(' ')]
+result = []
+for i in inp:
+    result.append(str(wrong_fibonachi(i)))
+
+print(' '.join(result))
--- Stepan_tasks/frequent_element.py
+++ Stepan_tasks/frequent_element.py
@@ -4,7 +4,7 @@
 
 inp = input().strip()  # входные данные (список чисел)
 inp = [int(i) for i in inp.split(' ')]
-unique_dict = {i: inp.count(i) for i in inp}
+unique_dict = {i: inp.count(i) for i in inp}  # делаем словарик, так как в нем получаем уникальные элементы ключей
 
 result = list(unique_dict.keys())[0]
 for i in unique_dict:
