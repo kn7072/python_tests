9f1212bb418c1a766b8af4a2953b11e7adbb7152
NikitaFedyanin <40120863+NikitaFedyanin@users.noreply.github.com>
2019-08-02 22:59:17 +0400

    новый задачи

---
+++ Stepan_tasks/frequent_element.py
@@ -0,0 +1,16 @@
+"""
+Дан массив a из n целых чисел. Напишите программу, которая найдет число, которое чаще других встречается в массиве.
+"""
+
+inp = input().strip()  # входные данные (список чисел)
+inp = [int(i) for i in inp.split(' ')]
+unique_dict = {i: inp.count(i) for i in inp}
+
+result = list(unique_dict.keys())[0]
+for i in unique_dict:
+    if i > result:
+        result = i
+        if unique_dict[i] > unique_dict[result]:
+            result = i
+
+print(result)
