32c3fc6fb3aee5e29d0c0eaa6eb32c29bdaa3662
NikitaFedyanin <40120863+NikitaFedyanin@users.noreply.github.com>
2019-08-03 15:21:31 +0400

    новый задачи

---
+++ Stepan_tasks/sql_joij.py
@@ -0,0 +1,47 @@
+"""
+Аналог sql join
+"""
+
+
+def sql_join(table1, table2, action):
+    result = []
+    tab1_id = [i[0] for i in table1]
+    tab2_id = [i[0] for i in table2]
+    if action == 'INNER':
+        for i in range(len(table1)):
+            if table1[i][0] == table2[i][0]:
+                result.append(table1[i] + table2[i][1:])
+    if action == 'LEFT':
+        for i in range(len(table1)):
+            if table1[i][0] == table2[i][0]:
+                result.append(table1[i] + table2[i][1:])
+            else:
+                result.append(table1[i] + ['NULL'])
+    if action == 'RIGHT':
+        for i in range(len(table2)):
+            if table2[i][0] == table1[i][0]:
+                result.append(table1[i] + table2[i][1:])
+            else:
+                result.append(table2[i][:1] + ['NULL'] + table2[i][1:])
+    if action == 'FULL':
+        for i in range(len(table2)):
+            if table1[i][0] == table2[i][0]:
+                result.append(table1[i] + table2[i][1:])
+            if table1[i][0] not in tab2_id:
+                result.append(table1[i] + ['NULL'])
+            if table2[i][0] not in tab1_id:
+                result.append(table2[i][:1] + ['NULL'] + table2[i][1:])
+    return ''.join([' '.join(i) + '\n' for i in result])
+
+
+with open('C:/for_test.txt', 'r') as f:
+    table1 = []
+    table2 = []
+    for i in range(int(f.readline().replace('\n', ''))):
+        table1.append(f.readline().replace('\n', '').split(' '))
+    for i in range(int(f.readline().replace('\n', ''))):
+        table2.append(f.readline().replace('\n', '').split(' '))
+    action = f.readline()
+
+result = sql_join(table1, table2, action)
+print(result)
